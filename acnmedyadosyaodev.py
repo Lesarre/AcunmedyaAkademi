9fbc60076ba433451f129feb3f92d57a5b1af579
Lesarre <latifemres@gmail.com>
2025-03-08 15:34:11 +0300

    Update acnmedyadosyaodev.py

--- acnmedyadosyaodev.py
+++ acnmedyadosyaodev.py
@@ -6,15 +6,15 @@ with open ("metin.txt","r") as dosya:
     icerik = dosya.read()
     print(icerik)
 
-satir1 = input("satırınızı giriniz:")
-satir2 = input("satırınızı giriniz:")
-satir3 = input("satırınızı giriniz:")
-satir4 = input("satırınızı giriniz:")
-satir5 = input("satırınızı giriniz:")
+satir1 = input("satırınızı giriniz:" + "\n")
+satir2 = input("satırınızı giriniz:"  + "\n")
+satir3 = input("satırınızı giriniz:"  + "\n")
+satir4 = input("satırınızı giriniz:"  + "\n")
+satir5 = input("satırınızı giriniz:"  + "\n")
 
 with open ("metin2.txt","a") as dk:
     dk.write(satir1 +  satir2 +  satir3 +  satir4 +  satir5)
-
+    
 with open ("metin2.txt","r") as dkk:
     satirlar = dkk.readlines()
     print(satirlar)
